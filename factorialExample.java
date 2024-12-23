af9a6ea8c17a83cd35f87973cb9dceb46d6478f6
sravanimankala <mankalasravani661@gmail.com>
2024-12-23 01:45:52 -0800

    Add files via upload

---
+++ factorialExample.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.unit.*;
+class factorialExample{
+public static void main(String args[])
+{
+int i,fact=1;
+int number=5;
+for(i=1;i<=n;i++);{
+fact=fact*i
+}
+system.out.println("factorial of"+number+"is:"+fact);
+}
+}
